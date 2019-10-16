d49288f600b74f38897e56ad640202d145fd578d
tom <tome_andrade_silva@hotmail.com>
2019-10-16 18:58:22 +0100

    Nothing

--- common/database.py
+++ common/database.py
@@ -1,6 +1,6 @@
 import pymongo
+import os
 from typing import Dict
-
 
 
 class Database:
@@ -14,13 +14,15 @@ class Database:
     @staticmethod
     def find(collection: str, query: Dict) -> pymongo.cursor:
         return Database.DATABASE[collection].find(query)
+
     @staticmethod
     def find_one(collection: str, query: Dict) -> Dict:
         return Database.DATABASE[collection].find_one(query)
-    
+
     @staticmethod
     def update(collection: str, query: Dict, data: Dict) -> None:
         return Database.DATABASE[collection].update(query, data, upsert=True)
+
     @staticmethod
     def remove(collection: str, query: Dict) -> None:
         return Database.DATABASE[collection].remove(query)
